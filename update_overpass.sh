33ecce682bed101c801e9a873a1c9eb731a7272a
Lakshadeep Naik <lakshadeep.naik@gmail.com>
2018-10-18 15:05:46 +0200

    Added brsu osm map

--- update_overpass.sh
+++ update_overpass.sh
@@ -2,7 +2,7 @@
 docker container stop overpass_container
 docker container rm overpass_container
 rm planet.osm.bz2
-cp test.osm planet.osm
+cp brsu.osm planet.osm
 bzip2 planet.osm
 docker build -t overpass .
 docker run --name overpass_container -d -p 8000:80 overpass:latest
